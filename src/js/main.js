821e322df5f41b627a845ce1f8412dad1d1c9c22
Tim Schulz <Tim.Schulz.lkg@fh-zwickau.de>
2023-03-08 09:26:52 +0100

    add animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,53 @@
 import "bootstrap";
+import "../scss/style.scss";
+
+//* Scroll Timeline Animation for Sponsors
+import "./scroll-timeline";
+
+const first = document.querySelector(".sponsor-row-1");
+const second = document.querySelector(".sponsor-row-2");
+
+const scrollTimeLine = new ScrollTimeline({
+  source: document.scrollingElement,
+});
+
+const firstAnimation = {
+  transform: ["translateX(-10%)", "translateX(10%)"],
+};
+const secondAnimation = {
+  transform: ["translateX(10%)", "translateX(-10%)"],
+};
+
+first.animate(firstAnimation, {
+  timeline: scrollTimeLine,
+});
+second.animate(secondAnimation, {
+  timeline: scrollTimeLine,
+});
+
+// Observer Animation
+const animatedImages = document.querySelectorAll(".animate-image");
+const animatedText = document.querySelectorAll(".animate-text");
+
+const animatedElements = [...animatedImages, ...animatedText];
+
+animatedElements.forEach((image) => {
+  image.classList.add("hidden");
+});
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  },
+  { threshold: 0 }
+);
+
+animatedElements.forEach((element) => {
+  observer.observe(element);
+});
